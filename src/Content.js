4cfc9fe6c1d4103341e1a23632148ea44d043255
jaimi-sheta <jaimisheta@gmail.com>
2023-04-27 18:50:03 -0400

    Feat: integrated backend services with UI

--- src/Content.js
+++ src/Content.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import SimpleTable from './components/table/table';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Loader from './components/loader/loader';
+import NoDataCard from './components/table/noDataCard';
 
-export default function Content() {
+export default function Content(props) {
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    const submission = props.submission?.map((submission) => {
+      return createData(submission.data);
+    })
+    if (submission?.length > 0) {
+      setRows(submission);
+    }
+
+  }, [props.submission]);
+
+  function createData(data) {
+    return { email: data.email, firstName: data.firstName, lastName: data.lastName };
+  }
+
   return (
-    <Box sx={{marginTop: 3}}>
-      <Typography variant="h4">Liked Form Submissions</Typography>
-
-      <Typography variant="body1" sx={{fontStyle: 'italic', marginTop: 1}}>
-        TODO: List of liked submissions here (delete this line)
-      </Typography>
-    </Box>
+    <Box sx={{ mt: 3 }}>
+      <Typography sx={{ mb: 4 }} variant="h4">Liked Form Submissions</Typography>
+      {props.loader ? <Loader /> : rows.length === 0 ? <NoDataCard /> : (<SimpleTable rows={rows} />)}
+    </Box >
   );
 }
