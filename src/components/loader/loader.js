34d47002fb0445542d7eb21ebb73df457d991745
jaimi-sheta <jaimisheta@gmail.com>
2023-04-27 19:24:50 -0400

    Feat: added test cases for loader and snackbar

--- src/components/loader/loader.js
+++ src/components/loader/loader.js
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 
 export default function Loader() {
   return (
-    <Box sx={{ display: 'flex', pt: 8, justifyContent: 'center'}}>
+    <Box sx={{ display: 'flex', pt: 8, justifyContent: 'center' }}>
       <CircularProgress />
     </Box>
   );
